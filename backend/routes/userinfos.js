063cdaba8d763c992b3a69ab31696158d919fbf2
Qili <s1401037@gmail.com>
2023-02-22 13:08:14 +0800

    Update userinfos.js --add get all userinfos api

--- backend/routes/userinfos.js
+++ backend/routes/userinfos.js
@@ -1,4 +1,10 @@
 const router = require('express').Router();
 let Userinfo = require('../models/userinfo.model');
 
+router.route('/').get((req, res) => {
+  Userinfo.find()
+    .then(userinfos => res.json(userinfos))
+    .catch(err => res.status(400).json('Error' + err))
+})
+
 module.exports = router;
