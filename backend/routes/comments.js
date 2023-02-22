c77d91a0a9e910d4925a8f5d9abce1f25dcfd633
Qili <s1401037@gmail.com>
2023-02-22 13:34:04 +0800

    Update comments.js --add get comments by postId api

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -1,4 +1,10 @@
 const router = require('express').Router();
 let Comment = require('../models/comment.model');
 
+router.route('/:postId').get((req, res) => {
+  Comment.find({postId: req.params.postId})
+    .then(comments => res.json(comments))
+    .catch(err => res.status(400).json('Error' + err))
+})
+
 module.exports = router;
