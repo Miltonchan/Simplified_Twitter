c9669e6697afc8460d4f9a5478d0dc57d7529567
Qili <s1401037@gmail.com>
2023-04-05 23:19:15 +0800

    support search useraccounts by userId, username

--- backend/routes/useraccounts.js
+++ backend/routes/useraccounts.js
@@ -3,9 +3,21 @@ const router = require('express').Router();
 const Useraccount = require('../models/useraccount.model')
 
 router.route('/').get((req, res) => {
-  Useraccount.find()
-    .then(users => res.json(users))
-    .catch(err => res.status(400).json('Error' + err))
+  const username = req.query.username;
+  const userId = req.query.userId;
+  if (username) {
+    Useraccount.find({ username: new RegExp(username, 'i') })
+    .then(useraccounts => res.json(useraccounts)) 
+    .catch(err => res.status(400).json('Error' + err));
+  }else if (userId) {
+    Useraccount.find({ userId: userId})
+    .then(useraccounts => res.json(useraccounts))
+    .catch(err => res.status(400).json('Error' + err));
+  }else {
+    Useraccount.find()
+    .then(useraccounts => res.json(useraccounts))
+    .catch(err => res.status(400).json('Error' + err));
+  }
 })
 
 module.exports = router;
