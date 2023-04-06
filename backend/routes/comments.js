42f9a5d0556451f04373c8c595704908c1ecaa05
Qili <s1401037@gmail.com>
2023-04-06 11:13:58 +0800

    update message.js --support search by postId

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -1,10 +1,17 @@
 const router = require('express').Router();
 let Comment = require('../models/comment.model');
 
-router.route('/:postId').get((req, res) => {
-  Comment.find({postId: req.params.postId})
+router.route('/').get((req, res) => {
+  const postId = req.query.postId;
+  if (postId) {
+    Comment.find({ postId: postId })
+      .then(comments => res.json(comments))
+      .catch(err => res.status(400).json('Error' + err));
+  } else {
+    Comment.find()
     .then(comments => res.json(comments))
-    .catch(err => res.status(400).json('Error' + err))
+    .catch(err => res.status(400).json('Error' + err));
+  }
 })
 
 module.exports = router;
