6176aae753dc3f44e1ed906d3ac6f13dbae78c9a
Qili <s1401037@gmail.com>
2023-04-06 11:19:40 +0800

    update message.js --support get by rmId

--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -1,4 +1,17 @@
 const router = require('express').Router();
 let Message = require('../models/message.model');
 
+router.route('/').get((req, res) => {
+  const rmId = req.query.rmId;
+  if (rmId) {
+    Message.find({ rmId: rmId })
+      .then(message => res.json(message))
+      .catch(err => res.status(400).json('Error: ' + err));
+  }else {
+    Message.find()
+    .then(message => res.json(message))
+    .catch(err => res.status(400).json('Error: ' + err));
+  }
+})
+
 module.exports = router;
