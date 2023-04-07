db09f2df3270484d24fb38521b5ea236e8f094a7
Qili <s1401037@gmail.com>
2023-04-07 21:21:29 +0800

    update messages.js --minor changes

--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -5,20 +5,18 @@ router.route('/').get((req, res) => {
   const rmId = req.query.rmId;
   if (rmId) {
     Message.find({ rmId: rmId })
-      .then(message => res.json(message))
+      .then(messages => res.json(messages))
       .catch(err => res.status(400).json('Error: ' + err));
   }else {
     Message.find()
-    .then(message => res.json(message))
+    .then(messages => res.json(messages))
     .catch(err => res.status(400).json('Error: ' + err));
   }
 })
 
 router.route('/').post(async (req, res) => {
   const id = await Message.findOne().sort({ messageId: -1 }).limit(1)
-    .then(message => message['messageId']);
-
-  // res.json(id);
+    .then(message => message.messageId);
 
   const data = req.body;
   let message = new Message({
