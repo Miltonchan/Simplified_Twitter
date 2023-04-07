baa191c8724df118236f19df4cbb875a53b047c1
Qili <s1401037@gmail.com>
2023-04-07 15:21:03 +0800

    fix message.model.js --fix inappropriate schema

--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
     messageId:{type:Number, required:true, unique:true},
-    rmId:{type:Number, required:true, unique:true},
-    userId:{type:Number, required:true, unique:true}, 
-    message:{type:String, required:true, unique:true}
+    rmId:{type:Number, required:true, unique:false},
+    userId:{type:Number, required:true, unique:false}, 
+    message:{type:String, required:true, unique:false}
 },{
     timestamps: true 
 })
