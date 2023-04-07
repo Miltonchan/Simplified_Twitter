a20ccbe54d2ea201a05d20837a70501534621c6c
Qili <s1401037@gmail.com>
2023-04-07 14:43:20 +0800

    update chatroom.js --support get chatroom by firstUserId

--- backend/routes/chatrooms.js
+++ backend/routes/chatrooms.js
@@ -1,4 +1,17 @@
 const router = require('express').Router();
 let Chatroom = require('../models/chatroom.model');
 
+router.route('/').get((req, res) => {
+  const firstUserId = req.query.firstUserId;
+  if (firstUserId) {
+    Chatroom.find({ firstUserId: firstUserId })
+      .then(chatrooms => res.json(chatrooms))
+      .catch(err => res.status(400).json('Error: ' + err));
+  }else {
+    Chatroom.find()
+    .then(chatrooms => res.json(chatrooms))
+    .catch(err => res.status(400).json('Error: ' + err));
+  }
+})
+
 module.exports = router;
