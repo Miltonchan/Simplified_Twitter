2fc0bb99096559bdae066f9284caa04a62b64860
Qili <s1401037@gmail.com>
2023-04-07 21:33:03 +0800

    update posts.js --support get all posts, get post by postId --support post new post

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,4 +1,41 @@
 const router = require('express').Router();
 let Post = require('../models/post.model');
 
+router.route('/').get((req, res) => {
+  const postId = req.query.postId;
+  if (postId) {
+    Post.find({ postId: postId })
+      .then(posts => res.json(posts))
+      .catch(err => res.status(400).json('Error: ' + err));
+  }else {
+    Post.find()
+      .then(posts => res.json(posts))
+      .catch(err => res.status(400).json('Error: ' + err));
+  }
+})
+
+router.route('/').post(async (req, res) => {
+  const id = await Post.findOne().sort({ postId: -1 }).limit(1)
+  .then(post => post.postId);
+
+  const data = req.body;
+  let post = new Post({
+    postId: id + 1,
+    username: data.username,
+    content: data.content,
+    like: [],
+    dislike: [],
+    retweetBy: [],
+    retweet: false,
+  })
+
+  post.save((err) => {
+    if (err) {
+      res.status(400).json('Status: faild');
+    }else {
+      res.status(200).json('Status: success');
+    }
+  })
+})
+
 module.exports = router;
