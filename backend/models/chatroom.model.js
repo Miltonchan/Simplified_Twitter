ab8f2a573539b3b49eab9a2b9ab8d77f0a803b98
Qili <s1401037@gmail.com>
2023-04-08 22:17:17 +0800

    update chatroom.model.js --fix inappropriate schema contraints

--- backend/models/chatroom.model.js
+++ backend/models/chatroom.model.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const chatroomSchema = new mongoose.Schema({
     rmId:{type:Number, required:true, unique:true},
-    firstUserId:{type:Number, required:true, unique:true},
-    secUserId:{type:Number, required:true, unique:true},
+    firstUserId:{type:Number, required:true, unique:false},
+    secUserId:{type:Number, required:true, unique:false},
 })
 
 const Chatroom = mongoose.model('Chatroom', chatroomSchema);
