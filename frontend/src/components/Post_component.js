898430313aca208f95061e4ac3acbabdf4b9807f
封閉者 <97582406+AssoStudent@users.noreply.github.com>
2023-04-08 22:06:57 +0800

    Update pages

---
+++ frontend/src/components/Post_component.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Post_component.css'
+
+export default function Post_component() {
+  return (
+    <div class="center-container">
+      <div>
+        <p>Welcome to Beitter!</p>
+
+      </div>
+    </div>
+  )
+}
---
+++ frontend/src/components/Userinfo_component.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Userinfo_component.css'
+
+export default function Userinfo_component() {
+  return (
+    <div class="center-container">
+      <div>
+        <p>Welcome to Beitter!</p>
+
+      </div>
+    </div>
+  )
+}
