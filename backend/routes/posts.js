af5d21afb98e74f10d73c798b5f8dd4cf2cd2637
Qili <s1401037@gmail.com>
2023-04-08 21:39:13 +0800

    update posts.js --support like a post

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -12,8 +12,9 @@ router.route('/').get((req, res) => {
       .then(posts => res.json(posts))
       .catch(err => res.status(400).json('Error: ' + err));
   }
-})
+});
 
+// create new post
 router.route('/').post(async (req, res) => {
   const id = await Post.findOne().sort({ postId: -1 }).limit(1)
   .then(post => post.postId);
@@ -27,13 +28,38 @@ router.route('/').post(async (req, res) => {
     dislike: [],
     retweetBy: [],
     retweet: false,
-  })
+  });
 
   post.save((err) => {
     if (err) {
-      res.status(400).json('Status: faild');
+      res.status(400).json('Error: ' + err);
     }else {
       res.status(200).json('Status: success');
+    }
+  });
+});
+
+// like a post
+router.route('/like').post(async (req, res) => {
+  const data = req.body;
+  const filter = {
+    postId: data.postId,
+  }
+
+  let like =  await Post.findOne(filter)
+    .then(post => post.like);
+  
+  // do nothing if user already liked the post
+  if (like.includes(data.username)) {
+    res.json('Status: success');
+    return;
+  }
+
+  Post.updateOne(filter, { like: [...like, data.username] }, (err) => {
+    if (err) {
+      res.status(400).json('Error: ' + err);
+    }else {
+      res.json('Status: success');
     }
   })
 })
