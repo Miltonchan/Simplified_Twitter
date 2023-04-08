e72069990d565b268c235a87d14348b64e030f59
Qili <s1401037@gmail.com>
2023-04-08 22:18:03 +0800

    update chatrooms.js --support create new chatroom

--- backend/routes/chatrooms.js
+++ backend/routes/chatrooms.js
@@ -12,6 +12,39 @@ router.route('/').get((req, res) => {
     .then(chatrooms => res.json(chatrooms))
     .catch(err => res.status(400).json('Error: ' + err));
   }
-})
+});
+
+// create a new chatroom
+router.route('/').post(async (req, res) => {
+  const data = req.body;
+  const filter = {
+    firstUserId: data.firstUserId,
+    secUserId: data.secUserId,
+  }
+  const oldChatroom = await Chatroom.findOne(filter)
+    .then(chatroom => chatroom? true : false);
+
+  if (oldChatroom) {
+    res.status(400).json('Error: chatroom already exists');
+  }else {
+    const rmId = await Chatroom.findOne()
+      .sort({ rmId: -1 })
+      .then(chatroom => chatroom.rmId);
+
+    const chatroom = new Chatroom({
+      rmId: rmId + 1,
+      firstUserId: data.firstUserId,
+      secUserId: data.secUserId,
+    });
+
+    chatroom.save((err) => {
+      if (err) {
+        res.status(400).json('Error: ' + err);
+      }else {
+        res.status(200).json('Status: success');
+      }
+    });
+  }
+});
 
 module.exports = router;
