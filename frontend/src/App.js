bc3fc11366b44ca51d94ca38698e724a1e323263
封閉者 <97582406+AssoStudent@users.noreply.github.com>
2023-04-09 15:30:48 +0800

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,16 +10,24 @@ import Layout from './layout/layout';
 import Login from './components/Login_component';
 import Home from './components/Home_component';
 import Register from './components/Register_component';
+import Userinfo from './components/Userinfo_component';
+import Setting from './components/Setting_component';
+import Notification from './components/Notification_component';
+import Chatroom from './components/Chatroom_component';
 
 function App() {
   return (
     <Layout>
       <BrowserRouter>
         <Routes>
-          <Route index element={<Home />} />
+          <Route index element={<Notification />} />
           <Route path="/login/*" element = {<Login/>} />
           <Route path="/register/*" element = {<Register/>} />
           <Route path="/home/*" element = {<Home/>} />
+          <Route path="/userinfo/*" element = {<Userinfo/>} />
+          <Route path="/setting/*" element = {<Setting/>} />
+          <Route path="/notification/*" element = {<Notification/>} />
+          <Route path="/chatroom/*" element = {<Chatroom/>} />
         </Routes>
       </BrowserRouter>
     </Layout>
