53dd4773d4131264288bfa189dd2ac7147040cd7
MisTCC <125177407+MisTCC@users.noreply.github.com>
2023-04-09 18:17:27 +0800

    Add files via upload

--- frontend/src/layout/leftsidebar/leftsidebar.js
+++ frontend/src/layout/leftsidebar/leftsidebar.js
@@ -1,22 +1,37 @@
 import { BrowserRouter as Router, Route, Routes, useParams } from "react-router-dom";
 
 import React from "react";
-import MenuList from './leftsidebar_components/menulist';
+import {menulist_component} from './leftsidebar_components/menulist';
+import './leftsidebar.css'; // changed name ./leftsidebar.css from ./menulist.css
 
-class LeftSideBar extends React.Component {
-  render() {
+
+function LeftSideBar() {
     return(
-      <leftsidebar>
-        <Router>
-          <Routes>
-          <Route index element={<MenuList />} />
-          <Route path="/" element={MenuList}/>
-          <Route path="/main" element={MenuList}/>
-          <Route path="/home" element={MenuList}/>
-          </Routes>
-        </Router>
-      </leftsidebar>
-    );
+      <div className="leftsidebar">
+        <ul className="sidebarlist">
+        <li className="headicon"> 
+              {" "}
+              <div>icon here</div>
+              {" "}
+            </li> 
+        {menulist_component.map((val, key) =>{
+          return (
+            <li 
+              key={key}
+              className="row"
+              id={window.location.pathname == val.link ? "active" : " "}
+              onClick={() => {window.location.pathname = val.link}}> 
+              {" "}
+              <div id="icon">{val.icon}</div>{" "}<div id="title">{val.title}</div>
+            </li>
+          )
+        })}
+        </ul>
+        <button className="button">
+        Tweet
+        </button> 
+      </div>
+      
+    ); //head icons and button to be added
   }
-}
 export default LeftSideBar;
