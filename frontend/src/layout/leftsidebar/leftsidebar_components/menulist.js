21a9d6303aa94610bd9373ed4b269b310483aa1a
MisTCC <125177407+MisTCC@users.noreply.github.com>
2023-04-10 02:10:40 +0800

    Add files via upload

--- frontend/src/layout/leftsidebar/leftsidebar_components/menulist.js
+++ frontend/src/layout/leftsidebar/leftsidebar_components/menulist.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { useState } from "react";
 import HomeIcon from "./icons/homeIcon.png";
 import ExploreIcon from "./icons/exploreIcon.png";
 import NotificationIcon from "./icons/notificationIcon.png";
 import MessagesIcon from "./icons/messagesIcon.png";
 import ProfileIcon from "./icons/profileIcon.png";
 import MoreIcon from "./icons/moreIcon.png";
-
+import Popup from "./popup";
 import './menulist.css';
+import './popup.css';
 
 const h = 23;
 const w = 23;
@@ -41,31 +43,71 @@ const menulist_component = [
 ]
 
 export default function Menulist_component() {
+  const [buttonPopup, setButtonPopup] = useState(false);
+  const [twitter_content,setTwitter_content] = useState("");
+  const [Public_content,setPublic_content] = useState("Public");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const twitter =[twitter_content,Public_content] //to be added account information ????
+
+    console.log(twitter) // send to backend 
+    
+  }
     return(
       <div className="menulist-container">
         <ul className="sidebarlist">
-        <li className="headicon">
-              {" "}
-              <div>icon here</div>
-              {" "}
-            </li>
-        {menulist_component.map((val, key) =>{
-          return (
-            <li
-              key={key}
-              className="row"
-              id={window.location.pathname == val.link ? "active" : " "}
-              onClick={() => {window.location.pathname = val.link}}>
-              {" "}
-              <div id="icon">{val.icon}</div>{" "}<div id="title">{val.title}</div>
-            </li>
-          )
-        })}
+          <li className="headicon">
+            {" "}
+            <div>icon here</div>
+            {" "}
+          </li>
+          {menulist_component.map((val, key) => {
+            return (
+              <li
+                key={key}
+                className="row"
+                id={window.location.pathname == val.link ? "active" : " "}
+                onClick={() => { window.location.pathname = val.link; } }>
+                {" "}
+                <div id="icon">{val.icon}</div>{" "}<div id="title">{val.title}</div>
+              </li>
+            );
+          })}
         </ul>
-        <button className="sidebarlist-button">
-        Tweet
+        <button
+          className="sidebarlist-button"
+          onClick={() => setButtonPopup(true)}>
+          Tweet
         </button>
-      </div>
-
-    ); //head icons and button to be added
+        <main>
+        <Popup trigger={buttonPopup}>
+          <div className="twitter-form">
+            <form onSubmit={handleSubmit}> 
+              <label>
+              Twitter_content:
+              </label>
+              <textarea
+                required
+                placeholder="Enter twitter here..."
+                value={twitter_content}
+                onChange={(e) => setTwitter_content(e.target.value)}
+              />
+              <label>Set to Public/ Private </label>
+              <select
+              value={Public_content}
+              onChange={(e) => setPublic_content(e.target.value)}
+              >
+                  <option value="public">Public</option>
+                  <option value="private">Private</option>
+              </select>
+              <br />
+              <button className = "send-button">Send Twitter</button>
+              <button className = "close-button" onClick={ () =>setButtonPopup(false)}>close</button>
+            </form>
+          </div>
+        </Popup>
+        </main>
+        </div>
+        
+    ); //head icons and button to be added, send button and clos button to be added
 }
