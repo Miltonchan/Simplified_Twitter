aba7519a241610ecceb4a0c56002b51fb18b0c44
Qili <s1401037@gmail.com>
2023-04-09 19:35:38 +0800

    update adminaccounts.js --support admin login

--- backend/routes/adminaccounts.js
+++ backend/routes/adminaccounts.js
@@ -7,4 +7,17 @@ router.route('/').get((req, res) => {
       .catch(err => res.status(400).json('Error: ' + err));
 });
 
+// admin login
+router.route('/login').post((req, res) => {
+  const data = req.body;
+  const filter = {
+    username: data.username,
+    password: data.password,
+  }
+
+  Adminaccount.findOne(filter)
+    .then(adminaccount => adminaccount? res.json(adminaccount) : res.json('Error: wrong username or passoword'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
 module.exports = router;
