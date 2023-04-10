56480141e43bfb280589ede8a7fb46d879edc36f
封閉者 <97582406+AssoStudent@users.noreply.github.com>
2023-04-10 12:45:59 +0800

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import Register from './components/Register_component';
 import Userinfo from './components/Userinfo_component';
 import Setting from './components/Setting_component';
 import Notification from './components/Notification_component';
+import ChatroomList from './components/ChatroomList_component';
 import Chatroom from './components/Chatroom_component';
 
 function App() {
@@ -27,7 +28,8 @@ function App() {
           <Route path="/userinfo/*" element = {<Userinfo/>} />
           <Route path="/setting/*" element = {<Setting/>} />
           <Route path="/notification/*" element = {<Notification/>} />
-          <Route path="/chatroom/*" element = {<Chatroom/>} />
+          <Route path="/chatroom/*" element = {<ChatroomList/>} />
+          <Route path="/chatroom/chat/*" element = {<Chatroom/>} />
         </Routes>
       </BrowserRouter>
     </Layout>
---
+++ frontend/src/dialogs/AlertDialog.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './AlertDialog.css';
+
+import AcceptButton from '../icons/AcceptButton.png';
+import DeclineButton from '../icons/DeclineButton.png';
+
+function AlertDialog(props) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleYes() {
+    setIsOpen(false);
+    if (typeof props.onYes === 'function') {
+      props.onYes();
+    }
+  }
+
+  function handleNo() {
+    setIsOpen(false);
+    if (typeof props.onNo === 'function') {
+      props.onNo();
+    }
+  }
+
+  return (
+    <div className="faded-screen-background">
+      <div className="alert-dialog">
+        <div className="alert-dialog-topic">
+          {props.title}
+        </div>
+        <div className="alert-dialog-description">
+          <p>{props.description}</p>
+        </div>
+        <div className="alert-dialog-actions">
+          <div className="alert-dialog-button-container">
+            <img src={AcceptButton} className="alert-dialog-accept-button" onClick={handleYes} />
+            {props.yesLabel}
+          </div>
+          <div className="alert-dialog-button-container">
+            <img src={DeclineButton} className="alert-dialog-decline-button" onClick={handleNo} />
+            {props.noLabel}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+AlertDialog.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  onYes: PropTypes.func,
+  onNo: PropTypes.func,
+};
+
+export default AlertDialog
