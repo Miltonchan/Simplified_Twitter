d28f95890fdb6400a9836536cd95b42822f2491c
Qili <s1401037@gmail.com>
2023-04-11 22:22:33 +0800

    support user login

--- frontend/src/components/Login_component.js
+++ frontend/src/components/Login_component.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { UserContext } from '../App';
 import './Login_component.css';
 import '../icons/LoginBackground.png'
 
@@ -9,9 +10,39 @@ export default function Login_component() {
   const [loginComplete, setLoginComplete] = useState(false);
   const [animateComplete, setAnimateComplete] = useState(false);
   const [routeHome, setRouteHome] = useState(false);
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   const navigate = useNavigate();
 
-  const login = () => {
+  const getUseraccount = async (username, password) => {
+    const useraccount = await fetch('http://localhost:8000/useraccounts/login',
+    {
+      method: 'POST',
+      mode: 'cors',
+      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify({
+        "username": username,
+        "password": password,
+      })
+    })
+    .then(data => data.json());
+    return useraccount;
+  }
+
+  const getUserinfo = async (userId) => {
+    const userinfo = await fetch(`http://localhost:8000/userinfos?userId=${userId}`,
+    {
+      method: 'GET',
+      mode: 'cors',
+    })
+    .then(data => data.json());
+    return userinfo;
+  }
+
+  const login = async (e) => {
+    e.preventDefault();
     setLoginComplete(true);
     setAnimateComplete(false);
 
@@ -19,9 +50,22 @@ export default function Login_component() {
       setLoginComplete(false);
     }, 1400);
 
+    const useraccount = await getUseraccount(username, password);
+    if (useraccount.Error) {
+      alert(useraccount.Error);
+      return;
+    }
+    const userinfo = await getUserinfo(useraccount.userId);
+    localStorage.setItem('user', JSON.stringify({
+      'userinfo': userinfo,
+      'useraccount': useraccount,
+    }));
+
     setTimeout(() => {
       setAnimateComplete(true);
     }, 700);
+
+    return false;
   }
 
   const routeToHome = () => {
@@ -44,17 +88,17 @@ export default function Login_component() {
           </div>
           <div className={"login-form-container " + (animateComplete ? "invisible" : "")}>
             <div className="login-form-block">
-              <form action="" method="post" className="login-form">
+              <form action="" method="post" className="login-form" onSubmit={login}>
                 <div className="login-form">
                   <label for="username">Username: </label>
-                  <input type="text" name="username" id="username" required/>
+                  <input type="text" name="username" id="username" value={username} onChange={(e) => setUsername(e.target.value)}/>
                 </div>
                 <div className="login-form">
                   <label for="password">Password: </label>
-                  <input type="text" name="password" id="password" required/>
+                  <input type="text" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
                 </div>
                 <div className="login-form">
-                  <button onClick={login}>OK</button>
+                  <button type='submit'>OK</button>
                 </div>
              </form>
              <Link className="register" to ='/register'> Not yet register? Sign Up now! </Link>
