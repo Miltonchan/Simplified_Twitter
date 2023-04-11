747a31821990f710382ef05cf08546e6aed2d05f
Qili <s1401037@gmail.com>
2023-04-12 00:16:52 +0800

    update Tweet_component.js --support like, dislike a post

--- frontend/src/components/subcomponents/Tweet_component.js
+++ frontend/src/components/subcomponents/Tweet_component.js
@@ -6,15 +6,24 @@ const Tweet = () => {
   const user = JSON.parse(localStorage.getItem('user'));
 
   const fetchPosts = async () => {
-    const postData = await fetch('http://localhost:8000/posts',
+    let postData = await fetch('http://localhost:8000/posts',
       {
         method: 'GET',
         mode: 'cors'
       })
       .then(data => data.json());
-
+     
+    postData = await fetchComments(postData);
     setPosts(postData);
     console.log(postData);
+  }
+
+  const fetchComments = async (postData) => {
+    for (let i=0; i<postData.length; i++) {
+      postData[i].comment = await fetch('http://localhost:8000/comments')
+        .then(data => data.json());
+    }
+    return postData;
   }
 
   const likePosts = async (postId) => {
@@ -29,8 +38,44 @@ const Tweet = () => {
         })
       })
       .then(data => data.json())
+      .finally(fetchPosts)
+
       alert(res);
   }
+
+  const dislikePosts = async (postId) => {
+    const res = await fetch('http://localhost:8000/posts/dislike',
+      {
+        method: 'POST',
+        mode: 'cors',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({
+          "postId": postId,
+          "username": user.userinfo.username
+        })
+      })
+      .then(data => data.json())
+      .finally(fetchPosts)
+
+      alert(res);
+  }
+
+  // const retweetPosts = async (postId) => {
+  //   const res = await fetch('http://localhost:8000/posts/retweet',
+  //     {
+  //       method: 'POST',
+  //       mode: 'cors',
+  //       headers: {'Content-Type':'application/json'},
+  //       body: JSON.stringify({
+  //         "postId": postId,
+  //         "username": user.userinfo.username
+  //       })
+  //     })
+  //     .then(data => data.json())
+  //     .finally(fetchPosts)
+
+  //     alert(res);
+  // }
 
   useEffect(() => {
     fetchPosts();
@@ -39,41 +84,41 @@ const Tweet = () => {
   return (
     <div className="tweet-page">
       <div className="tweet-topicbar">
-        <h7>Tweet</h7>
+        <h6>Tweet</h6>
         <div className="description-text">
           Take a look in today's SAO tweet.
         </div>
       </div>
       <div className="tweet-section">
-        {posts.map((val, key) => {
+        {posts.map((val, index) => {
           return (
-            <div className="tweet-block-container">
+            <div key={index} className="tweet-block-container">
               <div className="tweet-block">
                 <div className="tweet-block-user-bar">
                   <div className="tweet-block-user-name-block">
-                    <h7>{val.username}</h7>
+                    <h6>{val.username}</h6>
                   </div>
                   <div className="tweet-block-user-bar-block">
                     <h2>{val.username}</h2>
                   </div>
                 </div>
                 <div className="tweet-block-content-section">
-                  <div className="tweet-block-content" key={key}>
+                  <div className="tweet-block-content">
                     {val.content}
                   </div>
                 </div>
                 <div className="tweet-block-action-bar">
                   <div onClick={() => likePosts(val.postId)} className="tweet-block-action-block">
-                    like
+                    like {val.like.length}
+                  </div>
+                  <div onClick={() => dislikePosts(val.postId)} className="tweet-block-action-block">
+                    dislike {val.dislike.length}
                   </div>
                   <div className="tweet-block-action-block">
-                    dislike
+                    retweet {val.retweetBy.length}
                   </div>
                   <div className="tweet-block-action-block">
-                    retweet
-                  </div>
-                  <div className="tweet-block-action-block">
-                    comment
+                    comment {val.comment.length}
                   </div>
                 </div>
               </div>
