a2abe9b5408f2c9c0f7cbbdbc5404506e3d6a387
封閉者 <97582406+AssoStudent@users.noreply.github.com>
2023-04-11 19:50:48 +0800

    Add files via upload

--- frontend/src/layout/rightsidebar/rightsidebar.js
+++ frontend/src/layout/rightsidebar/rightsidebar.js
@@ -1,11 +1,18 @@
 import { BrowserRouter as Router, Route, Routes, useParams } from "react-router-dom";
 
 import React from "react";
+import Search from './rightsidebar_components/search';
 
 class RightSideBar extends React.Component {
   render() {
     return(
       <rightsidebar>
+        <Router>
+          <Routes>
+          <Route index element={<Search />} />
+          <Route path="/*" element = {<Search/>} />
+          </Routes>
+        </Router>
       </rightsidebar>
     );
   }
---
+++ frontend/src/layout/rightsidebar/rightsidebar_components/search.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import './search.css';
+
+export default function Search_component() {
+  return (
+    <div>
+      Test
+    </div>
+  )
+}
