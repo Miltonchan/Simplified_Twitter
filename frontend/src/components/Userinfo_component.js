99c39d7bad7181e715cc5ac1821b86734bfd4a70
Qili <s1401037@gmail.com>
2023-04-12 17:57:22 +0800

    update Profile

--- frontend/src/components/Userinfo_component.js
+++ frontend/src/components/Userinfo_component.js
@@ -4,6 +4,9 @@ import './Userinfo_component.css';
 import AlertDialog from '../dialogs/AlertDialog';
 
 export default function Userinfo_component() {
+  const user = JSON.parse(localStorage.getItem('user'));
+  const [posts, setPosts] = useState([]);
+
   const [isFollowDialogOpen, setIsFollowDialogOpen] = useState(false);
 
   const handleFollowDialogClick = () => {
@@ -18,6 +21,23 @@ export default function Userinfo_component() {
     setIsFollowDialogOpen(false);
   };
 
+  const fetchSelfPosts = async () => {
+    let postData = await fetch(`http://localhost:8000/posts?username=${user.userinfo.username}`,
+    {
+      method: 'GET',
+      mode: 'cors'
+    })
+    .then(data => data.json());
+
+    setPosts(postData);
+    // console.log(postData);
+  }
+
+  useEffect(() => {
+    fetchSelfPosts();
+  }, []);
+
+
   return (
     <div className="userinfo-page">
       <div className="userinfo-topic">
@@ -29,22 +49,21 @@ export default function Userinfo_component() {
             <div className="userinfo-profile-header">
               <img src="https://picsum.photos/200" alt="User avatar" className="userinfo-avatar" />
               <div className="userinfo-userdetails">
-                <h1 className="uesrinfo-name">Kirito</h1>
-                <p className="userinfo-userid">@Kirito</p>
-                <p className="userinfo-bio">Beater.</p>
+                <h1 className="uesrinfo-name">{user.userinfo.username}</h1>
+                <p className="userinfo-userid">@{user.useraccount.userId}</p>
               </div>
             </div>
             <div className="userinfo-stats">
               <div className="userinfo-stat">
-                <p className="userinfo-count">100</p>
+                <p className="userinfo-count">{user.userinfo.follower.length}</p>
                 <p className="userinfo-label">Followers</p>
               </div>
               <div className="userinfo-stat">
-                <p className="userinfo-count">200</p>
+                <p className="userinfo-count">{user.userinfo.following.length}</p>
                 <p className="userinfo-label">Following</p>
               </div>
               <div className="userinfo-stat">
-                <p className="userinfo-count">300</p>
+                <p className="userinfo-count">{posts.length}</p>
                 <p className="userinfo-label">Tweets</p>
               </div>
             </div>
