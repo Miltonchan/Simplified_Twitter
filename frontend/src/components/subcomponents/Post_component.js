b82ef91a1d5d60f4f0d61b1d986226e7370543d9
Qili <s1401037@gmail.com>
2023-04-12 22:42:25 +0800

    implement add new post

--- frontend/src/components/subcomponents/Post_component.js
+++ frontend/src/components/subcomponents/Post_component.js
@@ -2,18 +2,40 @@ import React, { useState, useEffect } from 'react';
 import './Post_component.css';
 
 const Post = () => {
+  const [content, setContent] = useState('');
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  const handleSubmit = async () => {
+    if (!content) {
+      return false;
+    }
+
+    await fetch('http://localhost:8000/posts', {
+      method: 'POST',
+      mode: 'cors',
+      body: JSON.stringify({
+        'username': user.useraccount.username,
+        'content': content,
+      })
+    })
+
+    setContent('');
+    return false;
+  }
+
   return (
     <div className="post-page">
       <div className="post-topicbar">
         Post
       </div>
-      <div className="post-section">
+      <form onSubmit={handleSubmit} className="post-section">
         <div className="post-block">
           <div className="post-textarea-container">
             <textarea
               required
               placeholder="Enter twitter here..."
               className="post-textarea"
+              onChange={(e) => setContent(e.target.value)}
             />
           </div>
           <div className="post-toolbar">
@@ -21,11 +43,11 @@ const Post = () => {
               Tool
             </div>
             <div className="post-toolbar-block">
-              <button className="post-toolbar-btn">Send</button>
+              <button type='submit' className="post-toolbar-btn">Send</button>
             </div>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
