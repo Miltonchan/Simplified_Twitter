8870678a2404ddb23f3273169a4e773b103427e3
封閉者 <97582406+AssoStudent@users.noreply.github.com>
2023-04-12 20:34:06 +0800

    Add files via upload

--- frontend/src/components/subcomponents/UserList_component.js
+++ frontend/src/components/subcomponents/UserList_component.js
@@ -1,35 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import './UserList_component.css';
+import '../../dialogs/MessageDialog.css';
 
-const UserList = () => {
+const UserList = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleCancel() {
+    setIsOpen(false);
+    if (typeof props.onCancel === 'function') {
+      props.onCancel();
+    }
+  }
+
   return (
-    <div className="userlist-page">
-      <div className="userlist-row">
-        <div className="userlist-username">
-          Kirito
+    <div className="faded-screen-background" onDoubleClick={handleCancel}>
+      <div className="message-dialog">
+        <div className="message-dialog-topic">
+          User List
         </div>
-        <div className="userlist-btn-container">
-          <button className="userlist-btn">Action</button>
+        <div className="message-dialog-description">
+          <div className="userlist-page">
+            <div className="userlist-row">
+              <div className="userlist-username">
+                Kirito
+              </div>
+              <div className="userlist-btn-container">
+                <button className="userlist-btn">Action</button>
+              </div>
+            </div>
+            <div className="userlist-row">
+              <div className="userlist-username">
+                Beater
+              </div>
+              <div className="userlist-btn-container">
+                <button className="userlist-btn">Action</button>
+              </div>
+            </div>
+            <div className="userlist-row">
+              <div className="userlist-username">
+                Asuna
+              </div>
+              <div className="userlist-btn-container">
+                <button className="userlist-btn">Action</button>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="userlist-row">
-        <div className="userlist-username">
-          Beater
-        </div>
-        <div className="userlist-btn-container">
-          <button className="userlist-btn">Action</button>
-        </div>
-      </div>
-      <div className="userlist-row">
-        <div className="userlist-username">
-          Asuna
-        </div>
-        <div className="userlist-btn-container">
-          <button className="userlist-btn">Action</button>
-        </div>
+        <div className="message-dialog-bottom-bar"/>
       </div>
     </div>
   )
 }
+
+UserList.propTypes = {
+  onCancel: PropTypes.func,
+};
 
 export default UserList
