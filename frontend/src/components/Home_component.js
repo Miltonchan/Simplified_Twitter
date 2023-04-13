36dc92d8a3bc0b8a251bb445ce8e2b777a690ddf
封閉者 <97582406+AssoStudent@users.noreply.github.com>
2023-04-14 00:13:03 +0800

    Remove foryou/following tab in Home

--- frontend/src/components/Home_component.js
+++ frontend/src/components/Home_component.js
@@ -5,44 +5,44 @@ import Tweet from './subcomponents/Tweet_component';
 import ForYouTabContent from './subcomponents/Home_ForYou_component';
 import FollowingTabContent from './subcomponents/Home_Following_component';
 
-const Tab = () => {
-  const [activeTab, setActiveTab] = useState('For You');
-
-  const handleClick = (tabName) => {
-    setActiveTab(tabName);
-  };
-
-  return (
-    <div className="home-tab">
-      <div className="home-tab-bar">
-        <div className="home-tab-label">
-          <div
-            className={activeTab === 'For You' ? 'active' : ''}
-            onClick={() => handleClick('For You')}
-          >
-            For You
-          </div>
-        </div>
-        <div className="home-tab-label">
-          <div
-            className={activeTab === 'Following' ? 'active' : ''}
-            onClick={() => handleClick('Following')}
-          >
-            Following
-          </div>
-        </div>
-      </div>
-      <div className="home-section">
-        {activeTab === 'For You' && (
-          <ForYouTabContent/>
-        )}
-        {activeTab === 'Following' && (
-          <FollowingTabContent/>
-        )}
-      </div>
-    </div>
-  );
-};
+// const Tab = () => {
+//   const [activeTab, setActiveTab] = useState('For You');
+//
+//   const handleClick = (tabName) => {
+//     setActiveTab(tabName);
+//   };
+//
+//   return (
+//     <div className="home-tab">
+//       <div className="home-tab-bar">
+//         <div className="home-tab-label">
+//           <div
+//             className={activeTab === 'For You' ? 'active' : ''}
+//             onClick={() => handleClick('For You')}
+//           >
+//             For You
+//           </div>
+//         </div>
+//         <div className="home-tab-label">
+//           <div
+//             className={activeTab === 'Following' ? 'active' : ''}
+//             onClick={() => handleClick('Following')}
+//           >
+//             Following
+//           </div>
+//         </div>
+//       </div>
+//       <div className="home-section">
+//         {activeTab === 'For You' && (
+//           <ForYouTabContent/>
+//         )}
+//         {activeTab === 'Following' && (
+//           <FollowingTabContent/>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
 
 export default function Home_component() {
   return (
@@ -53,9 +53,6 @@ export default function Home_component() {
       <div className="home-main">
         <div className="home-section">
           <Tweet/>
-        </div>
-        <div className="home-section">
-          <Tab/>
         </div>
       </div>
     </div>
